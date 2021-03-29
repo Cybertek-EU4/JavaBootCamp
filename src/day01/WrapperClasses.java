e02e66071e2eec01eeeef6aa151a49b109eed6ee
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-29 12:44:01 -0400

    Day 01

---
+++ src/day01/WrapperClasses.java
@@ -0,0 +1,35 @@
+package day01;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        byte b = 10;
+        int i = b;
+
+
+        Byte b2 = 10;
+        Integer i2 = (int)b2;
+
+
+        double d = 1;
+
+        Double d2 = d;
+
+        double d3 = b2;
+
+
+
+
+        String str1 = "123";
+        int num1 =        Integer.parseInt(str1);
+        System.out.println(num1 +1);
+
+
+
+
+
+
+    }
+
+}
