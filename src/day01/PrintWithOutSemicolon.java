755b8e0a5c8eaca54cb22cccd871202c0afda438
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-29 12:21:19 -0400

    Day 01

---
+++ src/day01/PrintWithOutSemicolon.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class PrintWithOutSemicolon {
+
+    public static void main(String[] args) {
+
+     //   System.out.println("Hello World")
+
+/*
+        if( System.out.append("Hello World")  != null  ){
+
+        }
+*/
+
+        /*
+        if( System.out.printf("Hello")  == null  ){
+
+        }
+         */
+
+        while( System.out.append("Hello World")  == null){
+
+        }
+
+
+
+
+
+    }
+
+}
