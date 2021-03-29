1df444641b5c418a795cdd0b32b840d911e18525
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-29 12:33:50 -0400

    Day 01

---
+++ src/day01/Primitives.java
@@ -0,0 +1,30 @@
+package day01;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 10;
+        int i = 10;
+        long l = 10;
+
+        int intNumber =  (int)b;  // implicit casting
+
+        byte byteNumber =  (byte)i; // explicit casting
+
+        char ch = '!';
+
+        System.out.println("ch = " + ch);
+
+        boolean  r1 = true;
+        boolean r2 = false;
+        boolean r3 =   10 > 9;
+
+
+
+
+
+    }
+
+}
--- src/day01/PrintWithOutSemicolon.java
+++ src/day01/PrintWithOutSemicolon.java
@@ -4,7 +4,7 @@ public class PrintWithOutSemicolon {
 
     public static void main(String[] args) {
 
-     //   System.out.println("Hello World")
+        System.out.println("Hello World");
 
 /*
         if( System.out.append("Hello World")  != null  ){
