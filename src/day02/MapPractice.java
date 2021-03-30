02eda03aa62176c7c8f76f915d88c9d4d617192f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-30 17:00:58 -0400

    Day 01

---
+++ src/day02/MapPractice.java
@@ -0,0 +1,15 @@
+package day02;
+
+import java.util.LinkedHashMap;
+
+public class MapPractice {
+
+    public static void main(String[] args) {
+        String[] namesGroup1 = {"Oliver", "Jack", "Noah", "Breanna", "Shay","James", "Conor", "Jake", "John"};
+        Integer[] salariesGroup1 = {11000, 105000, 95000, 85000, 100000, 90000, 78000, 118000, 98000};
+        LinkedHashMap<String, Integer> group1 = new LinkedHashMap<>();
+
+
+    }
+
+}
