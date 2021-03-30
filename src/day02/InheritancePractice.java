0a31156a4a2560485ed59cca4eee64263280e393
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-30 12:50:39 -0400

    Day 01

---
+++ src/day02/InheritancePractice.java
@@ -0,0 +1,57 @@
+package day02;
+
+
+class Car{
+    public String brand, model, color;
+    public double price;
+
+    public Car(String brand, String model, String color, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+}
+
+class Toyota extends Car{
+
+    public Toyota(String model, String color, double price){
+        super("Toyota", model, color, price );
+    }
+
+}
+
+class Mercedes extends Car{
+
+    public Mercedes( String model, String color, double price) {
+        super("Mercedes", model, color, price);
+    }
+
+}
+
+class BMW extends Car{
+
+    public BMW( String model, String color, double price) {
+        super("BMW", model, color, price);
+    }
+}
+
+public class InheritancePractice {
+
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry", "White", 15000);
+
+        System.out.println(toyota);
+    }
+}
